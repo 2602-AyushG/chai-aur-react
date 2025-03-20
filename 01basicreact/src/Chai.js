d42e9929fca8bbe3fc758920c353fbba24887d50
Ayush Gupta <ayushgupta@Ayushs-MacBook-Air.local>
2025-03-20 09:37:59 +0530

    basic setup

---
+++ 01basicreact/src/Chai.js
@@ -0,0 +1,8 @@
+function Chai(){
+    //keep capital letter convention else will give error
+    return(
+        <h1>chai is ready</h1>
+    )
+}
+
+export default Chai
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+import Chai from './chai.jsx'
+
+function App() {
+  return (
+    //fragmant- taaki multiple elements ko ek mai wrap krr sake because jsx dont allows multiple elements
+    <>
+    <Chai/>
+    </>
+  )
+}
+
+export default App
